34b8962601d2680d8399cc3d8bb020f48ec17332
Chung Hoang <chung.hoang@vmodev.com>
2019-08-29 15:04:43 +0700

    update for response json type

--- app/controllers/commons/common-controller.js
+++ app/controllers/commons/common-controller.js
@@ -2,7 +2,11 @@ class CommonController {
     findSuccess(res) {
         return (result) => {
             res.status(200);
-            res.json({'data': result});
+            res.json({
+                'error_code': 0,
+                'message': 'success',
+                'data': result
+            });
         }
     }
 
@@ -23,28 +27,44 @@ class CommonController {
     deleteSuccess(res) {
         return (result) => {
             res.status(200);
-            res.json({'deleted': result});
+            res.json({
+                'error_code': 0,
+                'message': 'success',
+                'data': {'deleted': result}
+            });
         }
     }
 
     serverError(res) {
         return (error) => {
             res.status(500);
-            res.json(error);
+            res.json({
+                'error_code': error.errorCode,
+                'message_code': error.message,
+                'data': {}
+            });
         }
     }
 
     findError(res) {
         return (error) => {
             res.status(404);
-            res.json(error);
+            res.json({
+                'error_code': error.errorCode,
+                'message_code': error.message,
+                'data': {}
+            });
         }
     }
 
     authenticationError(res) {
         return (error) => {
             res.status(401);
-            res.json(error);
+            res.json({
+                'error_code': 401,
+                'message_code': 'Authentication error',
+                'data': {}
+            });
         }
     }
 }
