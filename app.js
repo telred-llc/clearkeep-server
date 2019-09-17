be750964a5e1de40ec5f1822b07a9a400387b91e
Chung Hoang <chung.hoang@vmodev.com>
2019-09-17 14:05:35 +0700

    add cors to package and config to app

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require('cors');
+
+/* Add headers */
+app.use(cors());
 
 /* Database configuration */
 const database = require('./app/configs/database-config');
@@ -16,23 +20,4 @@ app.use(bodyParser.json());
 /* Router configuration */
 const REST_API_ROOT = '/api';
 app.use(REST_API_ROOT, require('./app/routes/router'));
-/* Add headers */
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 module.exports = app;
